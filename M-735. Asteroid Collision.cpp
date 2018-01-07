f44f455328aca0643effb2f8394353b4972d0b33
MarshallW906 <MaticWang@hotmail.com>
2018-01-07 15:37:43 +0800

    a more easier solution

--- M-735. Asteroid Collision.cpp
+++ M-735. Asteroid Collision.cpp
@@ -54,4 +54,17 @@ class Solution {
         }
         return ret;
     }
+
+    // Found in Discuss
+    vector<int> asteroidCollision_SimpleSolution(vector<int>& a) {
+        vector<int> s;  // use vector to simulate stack.
+        for (int i : a) {
+            while (!s.empty() && s.back() > 0 && s.back() < -i) s.pop_back();
+            if (s.empty() || i > 0 || s.back() < 0)
+                s.push_back(i);
+            else if (i < 0 && s.back() == -i)
+                s.pop_back();
+        }
+        return s;
+    }
 };
