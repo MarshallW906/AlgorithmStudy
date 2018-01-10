c4281a39dea3143deb907e87b2a95a0b610cfbfc
MarshallW906 <MaticWang@hotmail.com>
2018-01-10 16:54:29 +0800

    O(m + n)

---
+++ M-240. Search a 2D Matrix II.cpp
@@ -0,0 +1,24 @@
+#include "./stdc++.h"
+
+using namespace std;
+
+class Solution {
+   public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        if (m == 0) return false;
+        int n = matrix[0].size();
+        int i = 0, j = n - 1;
+
+        while (i < m && j >= 0) {
+            if (target == matrix[i][j])
+                return true;
+            else if (target > matrix[i][j]) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return false;
+    }
+};
