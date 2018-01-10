015f7ba94d1939893038a6cd77daebce9d41795c
MarshallW906 <MaticWang@hotmail.com>
2018-01-10 17:14:10 +0800

    O(n), with O(1) space

--- E-121. Best Time to Buy and Sell Stock.cpp
+++ E-121. Best Time to Buy and Sell Stock.cpp
@@ -4,11 +4,10 @@
 using namespace std;
 
 class Solution {
-public:
+   public:
     int maxProfit(vector<int>& prices) {
         int minIdx = 0, maxProfit = 0;
         int n = prices.size();
-        vector<int> f(n, 0);
         for (int i = 0; i < n; i++) {
             if (prices[i] < prices[minIdx]) {
                 minIdx = i;
