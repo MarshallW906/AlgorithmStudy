d42bfd347ae9e480c0a35bf80470beaaea393c43
MarshallW906 <MaticWang@hotmail.com>
2018-01-11 18:10:51 +0800

    count without '%' operation

--- E-412. Fizz Buzz.cpp
+++ E-412. Fizz Buzz.cpp
@@ -11,10 +11,13 @@ class Solution {
             buzz++;
             if (fizz == 3 && buzz == 5) {
                 ret.push_back("FizzBuzz");
+                fizz = buzz = 0;
             } else if (buzz == 5) {
                 ret.push_back("Buzz");
+                buzz = 0;
             } else if (fizz == 3) {
                 ret.push_back("Fizz");
+                fizz = 0;
             } else {
                 ostringstream ostr;
                 ostr << i;
