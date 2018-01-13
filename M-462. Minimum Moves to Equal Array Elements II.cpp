0eaea30edcd1e15994c31e2366ec60ec487daa3c
MarshallW906 <MaticWang@hotmail.com>
2018-01-13 19:21:44 +0800

    another math trick

---
+++ M-462. Minimum Moves to Equal Array Elements II.cpp
@@ -0,0 +1,13 @@
+#include "./stdc++.h"
+
+using namespace std;
+
+class Solution {
+   public:
+    int minMoves2(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size(), res = 0;
+        for (int i = 0; i < n / 2; ++i) res += (nums[n - 1 - i] - nums[i]);
+        return res;
+    }
+};
